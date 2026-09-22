7244f5d76525242d529c68a27ec6967241249c55
lisanaples <lisanaplesrealtor@gmail.com>
2026-09-22 16:27:27 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "steady-day-v13";
+const CACHE_NAME = "steady-day-v14";
 const APP_SHELL = [
   "./",
   "index.html",
-  "styles.css?v=13",
-  "app.js?v=13",
+  "styles.css?v=14",
+  "app.js?v=14",
   "manifest.webmanifest",
   "assets/steady-day-icon.svg"
 ];
